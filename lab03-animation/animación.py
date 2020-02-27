c8efa87710220d0deb8bba9d08c821db69c7af56
Joel.Romero <joel.romero@edu.uah.es>
2020-02-27 20:05:32 +0100

    Trabajo de la 3ª sesión

--- lab03-animation/animación.py
+++ lab03-animation/animación.py
@@ -2,10 +2,10 @@ import arcade
 
 arcade.open_window(800, 600, "Drawing Example")
 
-def draw_house():
-    arcade.draw_lrtb_rectangle_filled(300, 400, 300, 200, arcade.color.WOOD_BROWN)
-    arcade.draw_arc_filled(350, 300, 100, 100, arcade.color.FERRARI_RED, 0, 180, 0, 150)
-    arcade.draw_lrtb_rectangle_filled(340, 360, 250, 200, arcade.color.DARK_BROWN)
+def draw_house(x,y):
+    arcade.draw_lrtb_rectangle_filled(300+x, 400+x, 300+y, 200+y, arcade.color.WOOD_BROWN)
+    arcade.draw_arc_filled(350+x, 300+y, 100, 100, arcade.color.FERRARI_RED, 0, 180, 0, 150)
+    arcade.draw_lrtb_rectangle_filled(340+x, 360+x, 250+y, 200+y, arcade.color.DARK_BROWN)
 
 def draw_road():
     arcade.draw_lrtb_rectangle_filled(0, 1000, 149, 50, arcade.color.BLACK)
@@ -34,9 +34,9 @@ def draw_grass():
     arcade.draw_lrtb_rectangle_filled(0, 1000, 199, 150, arcade.color.APPLE_GREEN)
     arcade.draw_lrtb_rectangle_filled(0, 1000, 49, 0, arcade.color.APPLE_GREEN)
 
-def draw_mountain():
-    arcade.draw_triangle_filled(400, 200, 600, 600, 800, 200, arcade.color.DARK_BROWN)
-    arcade.draw_triangle_filled(500, 400, 600, 600, 700, 400, arcade.color.ANTIQUE_WHITE)
+def draw_mountain(x,y):
+    arcade.draw_triangle_filled(400+x, 200+y, 600+x, 600+y, 800+x, 200+y, arcade.color.DARK_BROWN)
+    arcade.draw_triangle_filled(500+x, 400+y, 600+x, 600+y, 700+x, 400+y, arcade.color.ANTIQUE_WHITE)
 
 def draw_sun(x,y):
     arcade.draw_point(x,y, arcade.color.AERO_BLUE, 5)
@@ -46,16 +46,21 @@ def on_draw(delta_time):
     arcade.start_render()
 
     on_draw.sun_x = 25
+    on_draw.house_x = 25
+    on_draw.mountain_x = 25
 
-    draw_house()
     draw_road()
     draw_grass()
-    draw_mountain()
-    draw_sun(on_draw.sun_x,0)
+    draw_house(0,0)
     draw_sun(0,0)
+    draw_mountain(0,0)
+    draw_sun(on_draw.sun_x,0)
+    draw_house(on_draw.house_x, 0)
+    draw_mountain(on_draw.mountain_x, 0)
 
-    on_draw.sun_x += 100
-
+    on_draw.sun_x += 1
+    on_draw.house_x += 1
+    on_draw.mountain_x +=1
 
 def main():
     arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
