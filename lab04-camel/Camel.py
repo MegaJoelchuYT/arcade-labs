3f9d40f3e21d109efacc577a7f913b99b7b204b1
Joel.Romero <joel.romero@edu.uah.es>
2020-03-05 19:18:47 +0100

    principio Lab4

--- lab04-camel/Camel.py
+++ lab04-camel/Camel.py
@@ -1,2 +1,8 @@
-import random
-import arcade
+def main():
+    print("Welcome to Camel!")
+    print("You have stolen a camel to make your way across the great Mobi desert.")
+    print("The natives want their camel back and are chasing you down!"
+          " Survive your desert trek and out run the natives.")
+
+
+main()
