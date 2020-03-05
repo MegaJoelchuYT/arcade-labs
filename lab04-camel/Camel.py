724982317cb066d580f2be770dc8d1a5474befc0
Joel.Romero <joel.romero@edu.uah.es>
2020-03-05 19:38:49 +0100

    principio Lab4

--- lab04-camel/Camel.py
+++ lab04-camel/Camel.py
@@ -3,6 +3,17 @@ def main():
     print("You have stolen a camel to make your way across the great Mobi desert.")
     print("The natives want their camel back and are chasing you down!"
           " Survive your desert trek and out run the natives.")
-
+    done = False
+    while done == False:
+        print("A. Drink from your canteen.")
+        print("B. Ahead moderate speed.")
+        print("C. Ahead full speed.")
+        print("D. Stop for the night.")
+        print("E. Status check.")
+        print("Q. Quit.")
+        user_choice = str(input("What is your choice? "))
+        if user_choice.upper() == "Q":
+            print("You have quit the game")
+            done = True
 
 main()
