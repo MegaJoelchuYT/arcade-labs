9489431e7fbaf7d293222f85b24204dbfaf04c1b
Joel Romero <joel.romero@edu.uah.es>
2020-03-23 19:22:46 +0100

    Fixing my control exercise

--- lab07-control/Keyboard Control.py
+++ lab07-control/Keyboard Control.py
@@ -1,11 +1,9 @@
-""" Lab 7 - User Control """
-
 import arcade
 
-# --- Constants ---
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
 MOVEMENT_SPEED = 3
+
 
 class Ball:
     def __init__(self, position_x, position_y, change_x, change_y, radius, color):
@@ -21,12 +19,6 @@ class Ball:
     def draw(self):
         """ Draw the balls with the instance variables we have. """
         arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
-        arcade.set_background_color(arcade.color.DARK_BLUE)
-        arcade.start_render()
-        arcade.draw_rectangle_filled(self.position_x, self.position_y, 200, 85, arcade.color.RED)
-        arcade.draw_rectangle_filled(self.position_x, self.position_y, 125, 75, arcade.color.RED)
-        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, arcade.color.ASH_GREY)
-        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, arcade.color.ASH_GREY)
 
     def update(self):
         # Move the ball
@@ -45,6 +37,36 @@ class Ball:
 
         if self.position_y > SCREEN_HEIGHT - self.radius:
             self.position_y = SCREEN_HEIGHT - self.radius
+
+
+class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+
+        # Call the parent class's init function
+        super().__init__(width, height, title)
+
+        # Make the mouse disappear when it is over the window.
+        # So we just see our object, not the pointer.
+        self.set_mouse_visible(False)
+
+        arcade.set_background_color(arcade.color.ASH_GREY)
+
+        # Create our ball
+        self.ball = Ball(50, 50, 0, 0, 15, arcade.color.AUBURN)
+
+    def on_draw(self):
+        """ Called whenever we need to draw the window. """
+        arcade.set_background_color(arcade.color.DARK_BLUE)
+        arcade.start_render()
+        arcade.draw_rectangle_filled(200, 200, 200, 75, arcade.color.RED)
+        arcade.draw_rectangle_filled(200, 250, 125, 55, arcade.color.RED)
+        arcade.draw_circle_filled(250, 150, 25, arcade.color.ASH_GREY)
+        arcade.draw_circle_filled(150, 150, 25, arcade.color.ASH_GREY)
+        self.ball.draw()
+
+    def update(self, delta_time):
+        self.ball.update()
 
     def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
@@ -65,32 +87,8 @@ class Ball:
             self.ball.change_y = 0
 
 
-class MyGame(arcade.Window):
-    """ Our Custom Window Class"""
-
-    def __init__(self):
-        """ Initializer """
-
-        # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 7 - User Control")
-
-        # Make the mouse disappear when it is over the window.
-        # So we just see our object, not the pointer.
-        self.set_mouse_visible(False)
-
-        # Create our ball
-        self.ball = Ball(50, 50, 0, 0, 15, arcade.color.AUBURN)
-
-    def on_draw(self):
-        pass
-
-    def update(self, delta_time):
-        self.ball.update()
-
-
-
 def main():
-    window = Ball(300,300,50,50,50,arcade.color.DARK_BLUE)
+    window = MyGame(640, 480, "Drawing Example")
     arcade.run()
 
 
