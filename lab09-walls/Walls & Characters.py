48132afdb75795aa156bfc76dfa941803e08c914
Joel Romero <joel.romero@edu.uah.es>
2020-04-06 17:58:12 +0200

    Progress on Walls

--- lab09-walls/Walls & Characters.py
+++ lab09-walls/Walls & Characters.py
@@ -1,107 +1,128 @@
-""" Sprite Sample Program """
+"""
+Use sprites to scroll around a large screen.
 
+Simple program to show basic sprite usage.
+
+Artwork from http://kenney.nl
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.sprite_move_scrolling
+"""
+
+import random
 import arcade
+import os
 
-# --- Constants ---
-SPRITE_SCALING_BOX = 0.5
-SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING = 0.5
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Sprite Move with Scrolling Screen Example"
+
+# How many pixels to keep as a minimum margin between the character
+# and the edge of the screen.
+VIEWPORT_MARGIN = 40
 
 MOVEMENT_SPEED = 5
 
-VIEWPORT_MARGIN = 40
-
 
 class MyGame(arcade.Window):
-    """ This class represents the main window of the game. """
+    """ Main application class. """
 
-    def __init__(self):
-        """ Initializer """
-        # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
+    def __init__(self, width, height, title):
+        """
+        Initializer
+        """
+        super().__init__(width, height, title)
+
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
 
         # Sprite lists
         self.player_list = None
-        self.wall_list = None
+        self.coin_list = None
 
         # Set up the player
         self.player_sprite = None
-
-        # This variable holds our simple "physics engine"
+        self.wall_list = None
         self.physics_engine = None
-
-        # Manage the view port
-        self.view_left = 0
         self.view_bottom = 0
+        self.view_left = 0
 
     def setup(self):
-
-        # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
-
-        # Reset the view port
-        self.view_left = 0
-        self.view_bottom = 0
+        """ Set up the game and initialize the variables. """
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
 
-        # Reset the score
-        self.score = 0
-
-        # Create the player
-        self.player_sprite = arcade.Sprite("character1.png", SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 64
+        # Set up the player
+        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png", 0.4)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 270
         self.player_list.append(self.player_sprite)
 
-        # --- Manually place walls
+        # -- Set up several columns of walls
+        for x in range(200, 1650, 210):
+            for y in range(0, 1000, 64):
+                # Randomly skip a box so the player can find a way through
+                if random.randrange(5) > 0:
+                    wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+                    wall.center_x = x
+                    wall.center_y = y
+                    self.wall_list.append(wall)
 
-        # Manually create and position a box at 300, 200
-        wall = arcade.Sprite("boxCrate_double.png", SPRITE_SCALING_BOX)
-        wall.center_x = 300
-        wall.center_y = 200
-        self.wall_list.append(wall)
-
-        # Manually create and position a box at 364, 200
-        wall = arcade.Sprite("boxCrate_double.png", SPRITE_SCALING_BOX)
-        wall.center_x = 364
-        wall.center_y = 200
-        self.wall_list.append(wall)
-
-        # --- Place boxes inside a loop
-        for x in range(173, 650, 64):
-            wall = arcade.Sprite("boxCrate_double.png", SPRITE_SCALING_BOX)
-            wall.center_x = x
-            wall.center_y = 350
-            self.wall_list.append(wall)
-
-        # --- Place walls with a list
-        coordinate_list = [[400, 500],
-                           [470, 500],
-                           [400, 570],
-                           [470, 570]]
-
-        # Loop through coordinates
-        for coordinate in coordinate_list:
-            wall = arcade.Sprite("boxCrate_double.png", SPRITE_SCALING_BOX)
-            wall.center_x = coordinate[0]
-            wall.center_y = coordinate[1]
-            self.wall_list.append(wall)
-
-        # Create the physics engine. Give it a reference to the player, and
-        # the walls we can't run into.
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
+
+        # Set the viewport boundaries
+        # These numbers set where we have 'scrolled' to.
+        self.view_left = 0
+        self.view_bottom = 0
+
     def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
         arcade.start_render()
+
+        # Draw all the sprites.
         self.wall_list.draw()
         self.player_list.draw()
 
-    def update(self, delta_time):
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
         self.physics_engine.update()
 
         # --- Manage Scrolling ---
@@ -144,34 +165,14 @@ class MyGame(arcade.Window):
         # If we changed the boundary values, update the view port to match
         if changed:
             arcade.set_viewport(self.view_left,
-                                SCREEN_WIDTH + self.view_left - 1,
+                                SCREEN_WIDTH + self.view_left,
                                 self.view_bottom,
-                                SCREEN_HEIGHT + self.view_bottom - 1)
-
-    def on_key_press(self, key, modifiers):
-        """ Called whenever a key is pressed. """
-
-        if key == arcade.key.UP:
-            self.player_sprite.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.player_sprite.change_y = -MOVEMENT_SPEED
-        elif key == arcade.key.LEFT:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.player_sprite.change_x = MOVEMENT_SPEED
-
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
-
-        if key == arcade.key.UP or key == arcade.key.DOWN:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.player_sprite.change_x = 0
+                                SCREEN_HEIGHT + self.view_bottom)
 
 
 def main():
     """ Main method """
-    window = MyGame()
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
 
